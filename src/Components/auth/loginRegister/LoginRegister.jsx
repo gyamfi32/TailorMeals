0e8304ef820adc9342e72ef7f37b8fa527448d9e
Maria Arthur Gyamfi <abenajemfy@gmail.com>
2024-07-09 16:33:16 +0000

    Edited LoginRegister

--- src/Components/auth/loginRegister/LoginRegister.jsx
+++ src/Components/auth/loginRegister/LoginRegister.jsx
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./LoginRegister.css";
 import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
-import {
-  doSignInWithEmailAndPassword,
-  doSignInWithGoogle,
-  doCreateUserWithEmailAndPassword,
-} from "./auth";
-import { useAuth } from "../../../contexts/authContext";
-import { useNavigate } from "react-router-dom";
+// import {
+//   doSignInWithEmailAndPassword,
+//   doSignInWithGoogle,
+//   doCreateUserWithEmailAndPassword,
+// } from "./auth";
+// import { useAuth } from "../../../contexts/authContext";
+// import { useNavigate } from "react-router-dom";
 
 
 
 
 function LoginRegister () {
-  const { userLoggedIn } = useAuth();
-  const  navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [isSigningIn, setIsSigningIn] = useState(false);
+  // const { userLoggedIn } = useAuth();
+  // const  navigate = useNavigate();
+  // const [email, setEmail] = useState("");
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [isSigningIn, setIsSigningIn] = useState(false);
   const [action, setAction] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+
+//   useEffect(() => {
+//     if (userLoggedIn) {
+//       navigate("/");
+//     }
+//   }, [userLoggedIn, navigate]);
+
 
   const registerLink = (e) => {
     e.preventDefault();
@@ -34,68 +41,72 @@ function LoginRegister () {
     setErrorMessage("");
   };
 
-  const onSubmitLogin = async (e) => {
-    e.preventDefault();
-    if (!isSigningIn) {
-      setIsSigningIn(true);
-      try {
-        await doSignInWithEmailAndPassword(email, password);
-        setIsSigningIn(false);
-        navigate("/");
-      } catch (error) {
-        setIsSigningIn(false);
-        setErrorMessage("Error logging in: " + error.message);
-      }
-    }
-  };
+//   const onSubmitLogin = async (e) => {
+//     e.preventDefault();
+//     if (!isSigningIn) {
+//       setIsSigningIn(true);
+//       try {
+//         await doSignInWithEmailAndPassword(email, password);
+//         setIsSigningIn(false);
+//         navigate("/");
+//       } catch (error) {
+//         setIsSigningIn(false);
+//         setErrorMessage("Error logging in: " + error.message);
+//       }
+//     }
+//   };
 
-  const onSubmitRegister = async (e) => {
-    e.preventDefault();
-    if (!isSigningIn) {
-      setIsSigningIn(true);
-      try {
-        await doCreateUserWithEmailAndPassword(email, password);
-        setIsSigningIn(false);
-        navigate("/");
-      } catch (error) {
-        setIsSigningIn(false);
-        setErrorMessage("Error registering: " + error.message);
-      }
-    }
-  };
+//   const onSubmitRegister = async (e) => {
+//     e.preventDefault();
+//     if (!isSigningIn) {
+//       setIsSigningIn(true);
+//       try {
+//         await doCreateUserWithEmailAndPassword(email, password);
+//         setIsSigningIn(false);
+//         navigate("/");
+//       } catch (error) {
+//         setIsSigningIn(false);
+//         setErrorMessage("Error registering: " + error.message);
+//       }
+//     }
+//   };
 
-  const onGoogleSignIn = async (e) => {
-    e.preventDefault();
-    if (!isSigningIn) {
-      setIsSigningIn(true);
-      try {
-        await doSignInWithGoogle();
-        setIsSigningIn(false);
-      } catch (error) {
-        setIsSigningIn(false);
-        setErrorMessage("Error signing in with Google: " + error.message);
-      }
-    }
-  };
+//   const onGoogleSignIn = async (e) => {
+//     e.preventDefault();
+//     if (!isSigningIn) {
+//       setIsSigningIn(true);
+//       try {
+//         await doSignInWithGoogle();
+//         setIsSigningIn(false);
+//       } catch (error) {
+//         setIsSigningIn(false);
+//         setErrorMessage("Error signing in with Google: " + error.message);
+//       }
+//     }
+//   };
+
+//   if (userLoggedIn) {
+//     return null;
+//   }
 
   return (
     <div className="login-register-container">
-      {userLoggedIn && navigate("/")}
       <div className={`wrapper${action}`}>
         <div className="form-box login">
-          <form action="" onSubmit={onSubmitLogin}>
+          {/* <form onSubmit={onSubmitLogin}> */}
+          <form action="" >
             <h1>Login</h1>
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Email"
                 required
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  setErrorMessage("");
-                }}
-                aria-label="Email"
+                // value={email}
+                // onChange={(e) => {
+                //   setEmail(e.target.value);
+                //   setErrorMessage("");
+                // }}
+                // aria-label="Email"
               />
               <FaEnvelope className="icon" />
             </div>
@@ -104,12 +115,12 @@ function LoginRegister () {
                 type="password"
                 placeholder="Password"
                 required
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                  setErrorMessage("");
-                }}
-                aria-label="Password"
+                // value={password}
+                // onChange={(e) => {
+                //   setPassword(e.target.value);
+                //   setErrorMessage("");
+                // }}
+                // aria-label="Password"
               />
               <FaLock className="icon" />
             </div>
@@ -123,8 +134,8 @@ function LoginRegister () {
             </div>
 
             <button type="submit">Login</button>
-
-            <button onClick={onGoogleSignIn} disabled={isSigningIn}>
+            <button type="submit">
+            {/* <button onClick={onGoogleSignIn} disabled={isSigningIn}> */}
               Login with Google
             </button>
 
@@ -139,19 +150,20 @@ function LoginRegister () {
           </form>
         </div>
         <div className="form-box register">
-          <form action="" onSubmit={onSubmitRegister}>
+          <form>
+          {/* <form onSubmit={onSubmitRegister}> */}
             <h1>Registration</h1>
             <div className="input-box">
               <input
                 type="text"
                 placeholder="Username"
                 required
-                value={username}
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                  setErrorMessage("");
-                }}
-                aria-label="Username"
+                // value={username}
+                // onChange={(e) => {
+                //   setUsername(e.target.value);
+                //   setErrorMessage("");
+                // }}
+                // aria-label="Username"
               />
               <FaUser className="icon" />
             </div>
@@ -160,12 +172,12 @@ function LoginRegister () {
                 type="email"
                 placeholder="Email"
                 required
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  setErrorMessage("");
-                }}
-                aria-label="Email"
+                // value={email}
+                // onChange={(e) => {
+                //   setEmail(e.target.value);
+                //   setErrorMessage("");
+                // }}
+                // aria-label="Email"
               />
               <FaEnvelope className="icon" />
             </div>
@@ -174,12 +186,12 @@ function LoginRegister () {
                 type="password"
                 placeholder="Password"
                 required
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                  setErrorMessage("");
-                }}
-                aria-label="Password"
+                // value={password}
+                // onChange={(e) => {
+                //   setPassword(e.target.value);
+                //   setErrorMessage("");
+                // }}
+                // aria-label="Password"
               />
               <FaLock className="icon" />
             </div>
@@ -189,7 +201,8 @@ function LoginRegister () {
                 <input type="checkbox" />I agree to the terms & conditions
               </label>
             </div>
-            <button type="submit" disabled={isSigningIn}>
+            <button type="submit">
+            {/* <button type="submit" disabled={isSigningIn}> */}
               Register
             </button>
 
